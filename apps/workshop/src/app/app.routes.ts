6ee6e53fd4f0218d2a2b3fe84792ceb8e6265ed4
Fanis Prodromou <prodromouf@gmail.com>
2025-11-27 21:14:00 +0200

    chore: apply routing

--- apps/workshop/src/app/app.routes.ts
+++ apps/workshop/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('@workspace/feature-home').then((m) => m.HomePage),
+  },
+  {
+    path: 'catalogue',
+    loadComponent: () =>
+      import('@workspace/feature-catalogue').then((m) => m.CatalogueComponent),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
--- apps/workshop/src/app/app.ts
+++ apps/workshop/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
+import { HeaderComponent } from '@workspace/shared/ui/ui-header';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [RouterModule, HeaderComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {
-  protected title = 'workshop';
-}
+export class App {}
