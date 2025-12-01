93ecf4c307d81a3ec7a2785046f9e5bd1f7ef81d
Fanis Prodromou <prodromouf@gmail.com>
2025-12-01 14:14:35 +0200

    chore: define module boundaries

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,44 @@ export default [
               sourceTag: '*',
               onlyDependOnLibsWithTags: ['*'],
             },
+            {
+              "sourceTag": "type:feature",
+              "onlyDependOnLibsWithTags": [
+                "type:ui",
+                "type:util",
+                "type:model",
+                "type:feature",
+                "type:data-access"
+              ]
+            },
+            {
+              "sourceTag": "type:data-access",
+              "onlyDependOnLibsWithTags": [
+                "type:util",
+                "type:model",
+                "type:data-access"
+              ]
+            },
+            {
+              "sourceTag": "type:model",
+              "onlyDependOnLibsWithTags": ["type:model"]
+            },
+            {
+              "sourceTag": "type:ui",
+              "onlyDependOnLibsWithTags": [
+                "type:model",
+                "type:util",
+                "type:ui",
+              ]
+            },
+            {
+              "sourceTag": "scope:catalogue",
+              "onlyDependOnLibsWithTags": ["scope:catalogue", "scope:shared"]
+            },
+            {
+              "sourceTag": "scope:shared",
+              "onlyDependOnLibsWithTags": ["scope:shared"]
+            },
           ],
         },
       ],
--- libs/catalogue/types/README.md
+++ libs/catalogue/types/README.md
@@ -1,3 +1,12 @@
 # catalogue-types
 
 This library was generated with [Nx](https://nx.dev).
+
+These types are used in the shared/ui-product-card as well. According to the module boundary rules, these types should be moved in the scope:shared.
+
+Otherwise we get the error:
+/Users/profanis/projects/temp/angular.love/workshop/libs/shared/ui/ui-product-card/src/lib/product-card.component.ts
+12:1 error A project tagged with "scope:shared" can only depend on libs tagged with "scope:shared" @nx/enforce-module-boundaries
+
+Solution A: Define the scope:shared in this library (not recommended)
+Solution B: Move this library in shared library (recommended)
